8c70a81fddb270e07ae6a5fd83382c57bd874580
Sphinx85 <91307559+Sphinx85@users.noreply.github.com>
2022-01-05 21:06:10 +0300

    Клиентское окно чата. Поле для сохранения переписки Текстовое поле для ввода сообщения Кнопка отправки сообщения. Отправка сообщения так же по нажатию клавиши Enter

--- src/main/java/client/ApplicationController.java
+++ src/main/java/client/ApplicationController.java
@@ -1,4 +1,23 @@
 package client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ApplicationController {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    Button sendButton;
+
+    public void sendMessage(){
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
